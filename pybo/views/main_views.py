96a582bb70b5d6d0f8a7fd19996537f684f4ebc6
xmqtab <hsj99316@naver.com>
2024-12-09 23:25:13 +0900

    파이보 프로젝트 최초 커밋

---
+++ pybo/views/main_views.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, url_for
+from werkzeug.utils import redirect
+
+bp = Blueprint('main', __name__, url_prefix='/')
+
+
+@bp.route('/hello')
+def hello_pybo():
+    return 'Hello, Pybo!'
+
+
+@bp.route('/')
+def index():
+    return redirect(url_for('question._list'))
+
+
+@bp.route('/detail/<int:question_id>/')
+def detail(question_id): 
+    question = Question.query.get_or_404(question_id)   
+    return render_template('question/question_detail.html', question=question)
